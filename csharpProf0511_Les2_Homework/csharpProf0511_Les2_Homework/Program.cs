602409807e6b8a337ee0991dd8702de3db11b143
EduardHauer <eduard.hauer.genadiy@gmail.com>
2017-11-06 19:45:04 +0100

    add: -Homework 1):  -class Worker, WorkerA, WorkerB  -...

--- csharpProf0511_Les2_Homework/csharpProf0511_Les2_Homework/Program.cs
+++ csharpProf0511_Les2_Homework/csharpProf0511_Les2_Homework/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/*
+*    Eduard Hauer | www.vk.com/edhauer | eduard.hauer.genadiy@gmail.com
+*/
+
 namespace csharpProf0511_Les2_Homework
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World;");
+            // !Тут баг с заполнением массива (запускай через CTRL + F5)!
+
+            // Заполнение массива wList
+            WorkerList wList = new WorkerList(new Worker[10]);
+            for(int i = 0; i < 10; i++)
+            {
+                Worker w = new WorkerA(0);
+                Worker.RandomWorker(ref w);
+                wList.wList[i] = new WorkerA(w);
+            }
+
+
+            // Считования из wList.
+            foreach(var item in wList)
+            {
+                Console.WriteLine(item);
+            }
 
             Console.ReadLine();
         }
---
+++ csharpProf0511_Les2_Homework/csharpProf0511_Les2_Homework/Worker.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace csharpProf0511_Les2_Homework
+{
+    abstract class Worker
+    {
+        public double payment;
+
+        public Worker(double payment)
+        {
+            this.payment = payment;
+        }
+
+        /// <summary>
+        /// Среднемесячная заработная плата
+        /// </summary>
+        /// <returns>Среднемесячная заработная плата</returns>
+        abstract public double AverageMonthlyPay();
+
+        public static void RandomWorker(ref Worker w)
+        {
+            Random r = new Random();
+
+            switch(r.Next(1))
+            {
+                case 0:
+                    w = new WorkerA(r.Next(100));
+                    break;
+                case 1:
+                    w = new WorkerB(r.Next(20000));
+                    break;
+                default:
+                    w = new WorkerA(0);
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{payment}";
+        }
+    }
+
+    class WorkerA : Worker, IComparable
+    {
+        public WorkerA(double payment) : base(payment)
+        {
+        }
+
+        public WorkerA(Worker w) : base(w.payment)
+        {
+
+        }
+
+        /// <summary>
+        /// Среднемесячная заработная плата
+        /// </summary>
+        /// <returns>Среднемесячная заработная плата</returns>
+        public override double AverageMonthlyPay()
+        {
+            return 20.8 * 8 * payment;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (payment > (obj as Worker).payment) return -1;
+            else if (payment < (obj as Worker).payment) return 1;
+            return 0;
+        }
+    }
+
+    class WorkerB : Worker, IComparable
+    {
+        public WorkerB(double payment) : base(payment)
+        {
+        }
+
+        public WorkerB(Worker w) : base(w.payment)
+        {
+
+        }
+
+        /// <summary>
+        /// Среднемесячная заработная плата
+        /// </summary>
+        /// <returns>Среднемесячная заработная плата</returns>
+        public override double AverageMonthlyPay()
+        {
+            return payment;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (this.payment > (obj as Worker).payment) return -1;
+            else if (this.payment < (obj as Worker).payment) return 1;
+            return 0;
+        }
+    }
+
+    class WorkerList : IEnumerable
+    {
+        // массив сотрудников
+        public Worker[] wList;
+        
+        public WorkerList(params Worker[] wList)
+        {
+            this.wList = wList;
+        }
+
+        // вывод данных
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < wList.Length; i++)
+                yield return wList[i];
+        }
+    }
+}
