d97332d63ba83d8392e533534d733624b45c65d3
EduardHauer <eduard.hauer.genadiy@gmail.com>
2017-11-06 18:34:59 +0100

    add: -output: "Hello, World;"

---
+++ csharpProf0511_Les2_Homework/csharpProf0511_Les2_Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csharpProf0511_Les2_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World;");
+
+            Console.ReadLine();
+        }
+    }
+}
